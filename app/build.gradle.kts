a82aad7caf9d0e2b96386b18b0a91a81d2988775
Hidenobu Nagai <hidenobu.nagai.jp@gmail.com>
2026-08-15 17:06:06 +0900

    chore: add Material Components 1.13.0 dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,5 +35,6 @@ kotlin {
 
 dependencies {
     implementation("androidx.activity:activity:1.13.0")
+    implementation("com.google.android.material:material:1.13.0")
     testImplementation("junit:junit:4.13.2")
 }

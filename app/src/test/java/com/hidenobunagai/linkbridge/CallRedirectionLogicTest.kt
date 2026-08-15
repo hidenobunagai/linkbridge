5ea414429e821e70b75f549231f357688297cc47
Hidenobu Nagai <hidenobu.nagai.jp@gmail.com>
2026-08-15 20:00:24 +0900

    test: import assertTrue/assertFalse for freshness tests

--- app/src/test/java/com/hidenobunagai/linkbridge/CallRedirectionLogicTest.kt
+++ app/src/test/java/com/hidenobunagai/linkbridge/CallRedirectionLogicTest.kt
@@ -1,7 +1,9 @@
 package com.hidenobunagai.linkbridge
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class CallRedirectionLogicTest {
